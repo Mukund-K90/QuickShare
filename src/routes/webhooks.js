f88f0f3ec9f54b22a43deec3535540bf4793baa3
Mukund-K90 <koladiyamukund76@gmail.com>
2025-02-13 12:08:59 +0530

    COMMIT

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -22,7 +22,7 @@ router.post('/razorpay-webhook', async (req, res) => {
         const { event, payload } = req.body;
 
         console.log(`✅ Webhook Event: ${event}`);
-        console.log(`📌 Event Payload:`, JSON.stringify(payload.entity, null, 2));
+        console.log(`📌 Event Payload:`,payload);
 
         return res.status(200).json({ success: true, message: `Received event: ${event}` });
     } catch (error) {
